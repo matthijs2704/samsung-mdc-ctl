9825234848c80d7f9fdaadcb1ce961819202efd9
Matthijs Logemann <matthijs@logemann.dev>
2021-03-21 14:53:46 +0100

    :tada: Initial commit

---
+++ samsung_mdc_ctl/__main__.py
@@ -0,0 +1,66 @@
+# type: ignore[attr-defined]
+
+from typing import Optional
+
+import random
+from enum import Enum
+
+import typer
+from rich.console import Console
+from samsung_mdc_ctl import __version__
+from samsung_mdc_ctl.example import hello
+
+
+class Color(str, Enum):
+    white = "white"
+    red = "red"
+    cyan = "cyan"
+    magenta = "magenta"
+    yellow = "yellow"
+    green = "green"
+
+
+app = typer.Typer(
+    name="samsung-mdc-ctl",
+    help="Awesome `samsung-mdc-ctl` is a Python cli/package created with https://github.com/TezRomacH/python-package-template",
+    add_completion=False,
+)
+console = Console()
+
+
+def version_callback(value: bool):
+    """Prints the version of the package."""
+    if value:
+        console.print(
+            f"[yellow]samsung-mdc-ctl[/] version: [bold blue]{__version__}[/]"
+        )
+        raise typer.Exit()
+
+
+@app.command(name="")
+def main(
+    name: str = typer.Option(..., help="Name of person to greet."),
+    color: Optional[Color] = typer.Option(
+        None,
+        "-c",
+        "--color",
+        "--colour",
+        case_sensitive=False,
+        help="Color for name. If not specified then choice will be random.",
+    ),
+    version: bool = typer.Option(
+        None,
+        "-v",
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Prints the version of the samsung-mdc-ctl package.",
+    ),
+):
+    """Prints a greeting for a giving name."""
+    if color is None:
+        # If no color specified use random value from `Color` class
+        color = random.choice(list(Color.__members__.values()))
+
+    greeting: str = hello(name)
+    console.print(f"[bold {color}]{greeting}[/]")
